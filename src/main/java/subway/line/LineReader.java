08e33b9ed16063d54295ef877c0a4e902a3cd375
Giwan Kim <giwan.kim@gmail.com>
2024-07-08 08:44:18 +0900

    fix(line): 노선을 못 찾았을 경우 예외 메시지에 ID를 추가한다.

--- src/main/java/subway/line/LineReader.java
+++ src/main/java/subway/line/LineReader.java
@@ -16,6 +16,6 @@ public class LineReader {
   public Line readById(Long id) {
     return lineRepository
         .findById(id)
-        .orElseThrow((() -> new IllegalArgumentException("Line #" + id + " not found!")));
+        .orElseThrow((() -> new IllegalArgumentException("해당 노선 #" + id + "이 존재하지 않습니다.")));
   }
 }
