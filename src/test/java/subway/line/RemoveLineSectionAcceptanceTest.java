878a7a9a46ba6a4aa73bfdffd2081b92bfbbe680
Giwan Kim <giwan.kim@gmail.com>
2024-07-10 12:45:09 +0900

    test(section): 구간이 하나만 있는 경우 구간 제거 시 에러가 발생한다는 인수 테스트를 작성했다.

--- src/test/java/subway/line/RemoveLineSectionAcceptanceTest.java
+++ src/test/java/subway/line/RemoveLineSectionAcceptanceTest.java
@@ -40,7 +40,7 @@ class RemoveLineSectionAcceptanceTest extends AcceptanceTest {
         20);
   }
 
-  /** Given 노선에 등록된 구간이 존재하고 When 종점역 구간 제거를 요청하면 Then 해당 노선 조회 시 등록한 구간의 하행 종점역에서 제외된다. */
+  /** Given 구간이 하나 이상 등록되어 있고 When 종점역 구간 제거를 요청하면 Then 해당 노선 조회 시 등록한 구간의 하행 종점역에서 제외된다. */
   @DisplayName("지하철 구간을 제거한다.")
   @Test
   void removeLineSection() {
@@ -53,13 +53,27 @@ class RemoveLineSectionAcceptanceTest extends AcceptanceTest {
     노선_구간_삭제됨(response, lineId, stationId);
   }
 
-  /** Given 구간이 등록되어 있고 When 하행 종점역이 아닌 구간을 삭제 요청하면 Then 400 Bad Request 에러가 반환된다. */
+  /** Given 구간이 하나 이상 등록되어 있고 When 하행 종점역이 아닌 구간을 삭제 요청하면 Then 400 Bad Request 에러가 반환된다. */
   @DisplayName("최종 종점 역이 아닌 구간 제거 시 에러가 발생한다.")
   @Test
-  void removeLineSectionNotTerminalStation() {
+  void removeNonTerminalLineSectionShouldReturnError() {
     long lineId = 1;
     long stationId = 2;
     String uri = String.format("/lines/%d/sections", lineId);
+
+    ExtractableResponse<Response> response = 노선_구간_삭제_요청(stationId, uri);
+
+    노선_구간_삭제_실패함(response);
+  }
+
+  /** Given 하나의 구간만 등록되어 있고 When 구간을 삭제 요청하면 Then 400 Bad Request 에러가 반환된다. */
+  @DisplayName("구간이 하나만 존재할 때 구간 제거 시 에러가 발생한다.")
+  @Test
+  void removeLastLineSectionShouldReturnError() {
+    long lineId = 1;
+    long stationId = 2;
+    String uri = String.format("/lines/%d/sections", lineId);
+    노선_구간_삭제_요청(3, String.format("/lines/%d/sections", lineId));
 
     ExtractableResponse<Response> response = 노선_구간_삭제_요청(stationId, uri);
 
