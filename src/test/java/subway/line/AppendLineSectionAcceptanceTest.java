fce4c0c4822b67e5139d7a28711af740ab50cf69
Giwan Kim <giwan.kim@gmail.com>
2024-07-10 09:26:19 +0900

    reafactor(test): 구간 등록 인수테스트 step 추출한다.

--- src/test/java/subway/line/AppendLineSectionAcceptanceTest.java
+++ src/test/java/subway/line/AppendLineSectionAcceptanceTest.java
@@ -1,19 +1,14 @@
 package subway.line;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static subway.line.LineAcceptanceSteps.지하철_노선_조회_요청;
+import static subway.line.AppendLineSectionSteps.지하철_구간_등록됨;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import subway.support.AcceptanceTest;
 
@@ -43,32 +38,19 @@ class AppendLineSectionAcceptanceTest extends AcceptanceTest {
   @DisplayName("지하철 구간을 등록한다.")
   @Test
   void appendSection() {
+    long lineId = 1;
     long upStationId = 2;
     long downStationId = 3;
     int distance = 20;
     AppendLineSectionRequest request =
         new AppendLineSectionRequest(upStationId, downStationId, distance);
 
-    ExtractableResponse<Response> response =
-        RestAssured.given()
-            .log()
-            .all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(request)
-            .when()
-            .post("/lines/1/sections")
-            .then()
-            .log()
-            .all()
-            .extract();
+    ExtractableResponse<Response> response = AppendLineSectionSteps.지하철_구간_등록_요청(lineId, request);
 
-    assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-    ExtractableResponse<Response> lineResponse = 지하철_노선_조회_요청("/lines/1");
-    List<Long> stationIds = lineResponse.jsonPath().getList("stations.id", Long.class);
-    assertThat(stationIds.get(stationIds.size() - 1)).isEqualTo(downStationId);
+    지하철_구간_등록됨(response, lineId, downStationId);
   }
 
-  /** Given 새로운 구간의 상행역이 노선에 등록되어있는 하행 종점역이 아니고 When 구간 등록을 하면 Then 400 에러가 반환된다. */
+  /** Given 새로운 구간의 상행역이 노선에 등록되어있는 하행 종점역이 아니고 When 구간 등록을 하면 Then 400 Bad Request 에러가 반환된다. */
   @DisplayName("노선을 연장할 수 없는 구간을 등록 시 에러가 발생한다.")
   @Test
   @Disabled
@@ -76,7 +58,7 @@ class AppendLineSectionAcceptanceTest extends AcceptanceTest {
     throw new UnsupportedOperationException("테스트 작성 중");
   }
 
-  /** Given 구간의 하행 역이 이미 해당 노선에 등록되어 있으면 When 구간 등록을 하면 Then 400 에러가 반환된다. */
+  /** Given 구간의 하행 역이 이미 해당 노선에 등록되어 있으면 When 구간 등록을 하면 Then 400 Bad Request 에러가 반환된다. */
   @DisplayName("Cycle이 생길 수 있는 지하철 구간 등록 시 에러가 발생한다.")
   @Test
   @Disabled
---
+++ src/test/java/subway/line/AppendLineSectionSteps.java
@@ -0,0 +1,38 @@
+package subway.line;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static subway.line.LineAcceptanceSteps.지하철_노선_조회_요청;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+public class AppendLineSectionSteps {
+  private AppendLineSectionSteps() {}
+
+  public static ExtractableResponse<Response> 지하철_구간_등록_요청(
+      long lineId, AppendLineSectionRequest request) {
+    return RestAssured.given()
+        .log()
+        .all()
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .body(request)
+        .when()
+        .post("/lines/" + lineId + "/sections")
+        .then()
+        .log()
+        .all()
+        .extract();
+  }
+
+  public static void 지하철_구간_등록됨(
+      ExtractableResponse<Response> response, long lineId, long downStationId) {
+    assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    ExtractableResponse<Response> lineResponse = 지하철_노선_조회_요청("/lines/" + lineId);
+    List<Long> stationIds = lineResponse.jsonPath().getList("stations.id", Long.class);
+    assertThat(stationIds.get(stationIds.size() - 1)).isEqualTo(downStationId);
+  }
+}
